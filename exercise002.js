df0ab667b1d486ffb6d3c2b942029ab9753cdad1
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-05 22:47:48 +0100

    feat: Implemented exercise002.js async await fetch

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,24 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
+
+// async await fetch
+const fetchData = async (apiEndPoint) => {
+  try {
+    const res = await fetch(apiEndPoint);
+    const data = await res.json();
+    console.log(data);
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
 
 fetchData(jsonTypicode);
